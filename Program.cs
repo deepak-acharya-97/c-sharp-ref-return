e0bb0f7f40fcabff365365f25512333c3c8b33df
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-01-11 14:16:19 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace play_with_ref_return
+{
+    class Program
+    {
+        public static int[] Numbers={1,2,3,4};
+        public static ref int SomeThing()
+        {
+            return ref Numbers[0];
+        }
+        public static void P(dynamic message) => Console.WriteLine(message);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ref var x=ref SomeThing();
+            foreach (var item in Numbers)
+            {
+                P(item);
+            }
+            x=999;
+            foreach (var item in Numbers)
+            {
+                P(item);
+            }
+        }
+    }
+}
